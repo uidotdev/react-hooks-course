fcf29e3eb0410469512c76251f92e77d11b127e5
Tyler McGinnis <tylermcginnis33@gmail.com>
2019-07-21 21:18:18 -0600

    Code for 4-card-nav

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,34 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ThemeConsumer } from '../contexts/theme'
+import ThemeContext from '../contexts/theme'
 
 export default function Card ({ header, subheader, avatar, href, name, children }) {
+  const theme = React.useContext(ThemeContext)
+
   return (
-    <ThemeConsumer>
-      {(theme) => (
-        <div className={`card bg-${theme}`}>
-          <h4 className='header-lg center-text'>
-            {header}
-          </h4>
-          <img
-            className='avatar'
-            src={avatar}
-            alt={`Avatar for ${name}`}
-          />
-          {subheader && (
-            <h4 className='center-text'>
-              {subheader}
-            </h4>
-          )}
-          <h2 className='center-text'>
-            <a className='link' href={href}>
-              {name}
-            </a>
-          </h2>
-          {children}
-        </div>
+    <div className={`card bg-${theme}`}>
+      <h4 className='header-lg center-text'>
+        {header}
+      </h4>
+      <img
+        className='avatar'
+        src={avatar}
+        alt={`Avatar for ${name}`}
+      />
+      {subheader && (
+        <h4 className='center-text'>
+          {subheader}
+        </h4>
       )}
-    </ThemeConsumer>
+      <h2 className='center-text'>
+        <a className='link' href={href}>
+          {name}
+        </a>
+      </h2>
+      {children}
+    </div>
   )
 }
 
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ThemeConsumer } from '../contexts/theme'
+import ThemeContext from '../contexts/theme'
 import { NavLink } from 'react-router-dom'
 
 const activeStyle = {
@@ -7,38 +7,36 @@ const activeStyle = {
 }
 
 export default function Nav ({ toggleTheme }) {
+  const theme = React.useContext(ThemeContext)
+
   return (
-    <ThemeConsumer>
-      {(theme) => (
-        <nav className='row space-between'>
-          <ul className='row nav'>
-            <li>
-              <NavLink
-                to='/'
-                exact
-                activeStyle={activeStyle}
-                className='nav-link'>
-                  Popular
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/battle'
-                activeStyle={activeStyle}
-                className='nav-link'>
-                  Battle
-              </NavLink>
-            </li>
-          </ul>
-          <button
-            style={{fontSize: 30}}
-            className='btn-clear'
-            onClick={toggleTheme}
-          >
-            {theme === 'light' ? '🔦' : '💡'}
-          </button>
-        </nav>
-      )}
-    </ThemeConsumer>
+    <nav className='row space-between'>
+      <ul className='row nav'>
+        <li>
+          <NavLink
+            to='/'
+            exact
+            activeStyle={activeStyle}
+            className='nav-link'>
+              Popular
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/battle'
+            activeStyle={activeStyle}
+            className='nav-link'>
+              Battle
+          </NavLink>
+        </li>
+      </ul>
+      <button
+        style={{fontSize: 30}}
+        className='btn-clear'
+        onClick={toggleTheme}
+      >
+        {theme === 'light' ? '🔦' : '💡'}
+      </button>
+    </nav>
   )
 }
