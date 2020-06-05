20389752c146140bea18a22de9e465da186efd68
Fhillip Castillo <gerson.fhillip@gmail.com>
2020-06-05 03:17:58 -0400

    my results refactor to functional component step 1

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import { battle } from '../utils/api'
-import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
-import Card from './Card'
-import PropTypes from 'prop-types'
-import Loading from './Loading'
-import Tooltip from './Tooltip'
-import queryString from 'query-string'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { battle } from "../utils/api";
+import {
+  FaCompass,
+  FaBriefcase,
+  FaUsers,
+  FaUserFriends,
+  FaCode,
+  FaUser,
+} from "react-icons/fa";
+import Card from "./Card";
+import PropTypes from "prop-types";
+import Loading from "./Loading";
+import Tooltip from "./Tooltip";
+import queryString from "query-string";
+import { Link } from "react-router-dom";
 
-function ProfileList ({ profile }) {
+function ProfileList({ profile }) {
   return (
-    <ul className='card-list'>
+    <ul className="card-list">
       <li>
-        <FaUser color='rgb(239, 115, 115)' size={22} />
+        <FaUser color="rgb(239, 115, 115)" size={22} />
         {profile.name}
       </li>
       {profile.location && (
         <li>
           <Tooltip text="User's location">
-            <FaCompass color='rgb(144, 115, 255)' size={22} />
+            <FaCompass color="rgb(144, 115, 255)" size={22} />
             {profile.location}
           </Tooltip>
         </li>
@@ -26,93 +33,95 @@ function ProfileList ({ profile }) {
       {profile.company && (
         <li>
           <Tooltip text="User's company">
-            <FaBriefcase color='#795548' size={22} />
+            <FaBriefcase color="#795548" size={22} />
             {profile.company}
           </Tooltip>
         </li>
       )}
       <li>
-        <FaUsers color='rgb(129, 195, 245)' size={22} />
+        <FaUsers color="rgb(129, 195, 245)" size={22} />
         {profile.followers.toLocaleString()} followers
       </li>
       <li>
-        <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+        <FaUserFriends color="rgb(64, 183, 95)" size={22} />
         {profile.following.toLocaleString()} following
       </li>
     </ul>
-  )
+  );
 }
 
 ProfileList.propTypes = {
   profile: PropTypes.object.isRequired,
-}
+};
 
-export default class Results extends React.Component {
-  state = {
+export default function Results({ location }) {
+  const [state, _setState] = React.useState({
     winner: null,
     loser: null,
     error: null,
     loading: true
-  }
-  componentDidMount () {
-    const { playerOne, playerTwo } = queryString.parse(this.props.location.search)
+  });
+  const { winner, loser, error, loading } = state;
+  const [playerOne, setPlayerOne] = React.useState(() => {
+    const { playerOne } = queryString.parse(location.search);
+    return playerOne;
+  });
+  const [playerTwo, setPlayerTwo] = React.useState(() => {
+    const { playerTwo } = queryString.parse(location.search);
+    return playerTwo;
+  });
 
-    battle([ playerOne, playerTwo ])
+  React.useEffect(() => {
+    battle([playerOne, playerTwo])
       .then((players) => {
-        this.setState({
+        _setState({
           winner: players[0],
           loser: players[1],
           error: null,
-          loading: false
-        })
-      }).catch(({ message }) => {
-        this.setState({
-          error: message,
-          loading: false
-        })
+          loading: false,
+        });
       })
+      .catch(({ message }) => {
+        _setState({
+          error: message,
+          loading: false,
+        });
+      });
+  }, [playerOne, playerTwo]);
+
+  if (loading === true) {
+    return <Loading text="Battling" />;
   }
-  render() {
-    const { winner, loser, error, loading } = this.state
 
-    if (loading === true) {
-      return <Loading text='Battling' />
-    }
-
-    if (error) {
-      return (
-        <p className='center-text error'>{error}</p>
-      )
-    }
-
-    return (
-      <React.Fragment>
-        <div className='grid space-around container-sm'>
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Winner'}
-            subheader={`Score: ${winner.score.toLocaleString()}`}
-            avatar={winner.profile.avatar_url}
-            href={winner.profile.html_url}
-            name={winner.profile.login}
-          >
-            <ProfileList profile={winner.profile}/>
-          </Card>
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Loser'}
-            subheader={`Score: ${loser.score.toLocaleString()}`}
-            avatar={loser.profile.avatar_url}
-            name={loser.profile.login}
-            href={loser.profile.html_url}
-          >
-            <ProfileList profile={loser.profile}/>
-          </Card>
-        </div>
-        <Link
-          to='/battle'
-          className='btn dark-btn btn-space'>
-            Reset
-        </Link>
-      </React.Fragment>
-    )
+  if (error) {
+    return <p className="center-text error">{error}</p>;
   }
+
+  return (
+    <React.Fragment>
+      <div className="grid space-around container-sm">
+        <Card
+          header={winner.score === loser.score ? "Tie" : "Winner"}
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
+          <ProfileList profile={winner.profile} />
+        </Card>
+        <Card
+          header={winner.score === loser.score ? "Tie" : "Loser"}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          name={loser.profile.login}
+          href={loser.profile.html_url}
+        >
+          <ProfileList profile={loser.profile} />
+        </Card>
+      </div>
+      <Link to="/battle" className="btn dark-btn btn-space">
+        Reset
+      </Link>
+    </React.Fragment>
+  );
 }
